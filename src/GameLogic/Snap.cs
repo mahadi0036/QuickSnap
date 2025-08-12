e86af091c4e761dfb25b9970a0c94ffdaaa647bd
mahadi0036 <mohammadmahadi0036@gmail.com>
2025-08-12 16:30:23 +0600

    add player hit with left and right shift, with score decrement for miss hits

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -5,177 +5,120 @@ using SwinGameSDK;
 using NUnit.Framework;
 #endif 
 
-
 namespace CardGames.GameLogic
 {
-	/// <summary>
-	/// The Snap card game in which the user scores a point if they
-	/// click when the rank of the last two cards match.
-	/// </summary>
-	public class Snap
-	{
-		// Keep only the last two cards...
-		private readonly Card[] _topCards = new Card[2];
+    public class Snap
+    {
+        private readonly Card[] _topCards = new Card[2];
+        private readonly Deck _deck;
+        private readonly Timer _gameTimer;
+        private int _flipTime = 1000;
+        private int[] _score = new int[2];
+        private bool _started = false;
 
-		// Have a Deck of cards to play with.
-		private readonly Deck _deck;
+        public Snap()
+        {
+            _deck = new Deck();
+            _gameTimer = SwinGame.CreateTimer();
+        }
 
-		// Use a timer to allow the game to draw cards at timed intervals
-		private readonly Timer _gameTimer;
+        public Card TopCard
+        {
+            get { return _topCards[1]; }
+        }
 
-		// The amount of time that must pass before a card is flipped?
-		private int _flipTime = 1000;
+        public bool CardsRemain
+        {
+            get { return _deck.CardsRemaining > 0; }
+        }
 
-		// the score for the 2 players
-		private int[] _score = new int[2];
+        public int FlipTime
+        {
+            get { return _flipTime; }
+            set { _flipTime = value; }
+        }
 
-		private bool _started = false;
+        public bool IsStarted
+        {
+            get { return _started; }
+        }
 
-		/// <summary>
-		/// Create a new game of Snap!
-		/// </summary>
-		public Snap ()
-		{
-			_deck = new Deck ();
-		}
+        public void Start()
+        {
+            if (!IsStarted)
+            {
+                _started = true;
+                _deck.Shuffle();
+                FlipNextCard();
+                _gameTimer.Start();
+            }
+        }
 
-		/// <summary>
-		/// Gets the card on the top of the "flip" stack. This card will be face up.
-		/// </summary>
-		/// <value>The top card.</value>
-		public Card TopCard
-		{
-			get
-			{
-				return _topCards [1];
-			}
-		}
+        public void FlipNextCard()
+        {
+            if (_deck.CardsRemaining > 0)
+            {
+                _topCards[0] = _topCards[1];
+                _topCards[1] = _deck.Draw();
+                _topCards[1].TurnOver();
+            }
+        }
 
-		/// <summary>
-		/// Indicates if there are cards remaining in the Snap game's Deck.
-		/// The game is over when there are no cards remaining.
-		/// </summary>
-		/// <value><c>true</c> if cards remain; otherwise, <c>false</c>.</value>
-		public bool CardsRemain
-		{
-			get { return _deck.CardsRemaining > 0; }
-		}
+        public void Update()
+        {
+            if (IsStarted && _gameTimer.Ticks > _flipTime)
+            {
+                _gameTimer.Reset();
+                FlipNextCard();
+            }
+        }
 
-		/// <summary>
-		/// Determines how many milliseconds need to pass before a new card is drawn
-		/// and placed on the top of the game's card stack.
-		/// </summary>
-		/// <value>The flip time.</value>
-		public int FlipTime
-		{
-			get { return _flipTime; }
-			set { _flipTime = value; }
-		}
+        public int Score(int idx)
+        {
+            if (idx >= 0 && idx < _score.Length)
+                return _score[idx];
+            return 0;
+        }
 
-		/// <summary>
-		/// Indicates if the game has already been started. You can only start the game once.
-		/// </summary>
-		/// <value><c>true</c> if this instance is started; otherwise, <c>false</c>.</value>
-		public bool IsStarted
-		{
-			get { return _started; }
-		}
+        public void PlayerHit(int player)
+        {
+            if (player >= 0 && player < _score.Length && IsStarted)
+            {
+                if (_topCards[0] != null && _topCards[0].Rank == _topCards[1].Rank)
+                {
+                    _score[player]++;
+                }
+                else
+                {
+                    _score[player] = Math.Max(0, _score[player] - 1);
+                }
+                _started = false;
+                _gameTimer.Stop();
+            }
+        }
 
-		/// <summary>
-		/// Start the Snap game playing!
-		/// </summary>
-		public void Start()
-		{
-			if ( ! IsStarted )			// only start if not already started!
-			{
-				_started = true;
-				_deck.Shuffle ();		// Return the cards and shuffle
+        #if DEBUG
+        public class SnapTests
+        {
+            [Test]
+            public void TestSnapCreation()
+            {
+                Snap s = new Snap();
+                Assert.IsTrue(s.CardsRemain);
+                Assert.IsNull(s.TopCard);
+            }
 
-				FlipNextCard ();		// Flip the first card...
-			}
-		}
-			
-		public void FlipNextCard()
-		{
-			if (_deck.CardsRemaining > 0)			// have cards...
-			{
-				_topCards [0] = _topCards [1];		// move top to card 2
-				_topCards [1] = _deck.Draw ();		// get a new top card
-				_topCards[1].TurnOver();			// reveal card
-			}
-		}
-
-		/// <summary>
-		/// Update the game. This should be called in the Game loop to enable
-		/// the game to update its internal state.
-		/// </summary>
-		public void Update()
-		{
-			//TODO: implement update to automatically slip cards!
-		}
-
-		/// <summary>
-		/// Gets the player's score.
-		/// </summary>
-		/// <value>The score.</value>
-		public int Score(int idx)
-		{
-			if ( idx >= 0 && idx < _score.Length )
-				return _score[idx]; 
-			else
-				return 0;
-		}
-
-		/// <summary>
-		/// The player hit the top of the cards "snap"! :)
-		/// Check if the top two cards' ranks match.
-		/// </summary>
-		public void PlayerHit (int player)
-		{
-			//TODO: consider deducting score for miss hits???
-			if ( player >= 0 && player < _score.Length &&  	// its a valid player
-				 IsStarted && 								// and the game is started
-				 _topCards [0] != null && _topCards [0].Rank == _topCards [1].Rank) // and its a match
-			{
-				_score[player]++;
-				//TODO: consider playing a sound here...
-			}
-
-			// stop the game...
-			_started = false;
-		}
-	
-		#region Snap Game Unit Tests
-		#if DEBUG
-
-		public class SnapTests
-		{
-			[Test]
-			public void TestSnapCreation()
-			{
-				Snap s = new Snap();
-
-				Assert.IsTrue(s.CardsRemain);
-				Assert.IsNull (s.TopCard);
-			}
-
-			[Test]
-			public void TestFlipNextCard()
-			{
-				Snap s = new Snap();
-
-				Assert.IsTrue(s.CardsRemain);
-				Assert.IsNull (s.TopCard);
-
-				s.FlipNextCard ();
-
-				Assert.IsNull (s._topCards [0]);
-				Assert.IsNotNull (s._topCards [1]);
-			}
-		}
-
-		#endif 
-		#endregion
-	}
+            [Test]
+            public void TestFlipNextCard()
+            {
+                Snap s = new Snap();
+                Assert.IsTrue(s.CardsRemain);
+                Assert.IsNull(s.TopCard);
+                s.FlipNextCard();
+                Assert.IsNull(s._topCards[0]);
+                Assert.IsNotNull(s._topCards[1]);
+            }
+        }
+        #endif
+    }
 }
-
--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -9,80 +9,77 @@ namespace CardGames
         public static void LoadResources()
         {
             Bitmap cards;
-            cards = SwinGame.LoadBitmapNamed ("Cards", "Cards.png");
-            SwinGame.BitmapSetCellDetails (cards, 82, 110, 13, 5, 53);      // set the cells in the bitmap to match the cards
+            cards = SwinGame.LoadBitmapNamed("Cards", "Cards.png");
+            SwinGame.BitmapSetCellDetails(cards, 82, 110, 13, 5, 53);
         }
 
-		/// <summary>
-		/// Respond to the user input -- with requests affecting myGame
-		/// </summary>
-		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
-		{
-			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
+        private static void HandleUserInput(Snap myGame)
+        {
+            SwinGame.ProcessEvents();
 
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
-				myGame.FlipNextCard ();
-			}
-		}
+            if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
+            {
+                if (!myGame.IsStarted)
+                    myGame.Start();
+                else
+                    myGame.FlipNextCard();
+            }
 
-		/// <summary>
-		/// Draws the game to the Window.
-		/// </summary>
-		/// <param name="myGame">The details of the game -- mostly top card and scores.</param>
-		private static void DrawGame(Snap myGame)
-		{
-			SwinGame.ClearScreen(Color.White);
+            if (myGame.IsStarted)
+            {
+                if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT) && 
+                    SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                {
+                    // TODO: Add sound effect
+                }
+                else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
+                {
+                    myGame.PlayerHit(0);
+                }
+                else if (SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                {
+                    myGame.PlayerHit(1);
+                }
+            }
+        }
 
-			// Draw the top card
-			Card top = myGame.TopCard;
-			if (top != null)
-			{
-				SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
-				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 350, 50);
-			}
-			else
-			{
-				SwinGame.DrawText ("No card played yet...", Color.RoyalBlue, 0, 20);
-			}
+        private static void DrawGame(Snap myGame)
+        {
+            SwinGame.ClearScreen(Color.White);
 
-			// Draw the back of the cards... to represent the deck
-			SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), 52, 160, 50);
+            Card top = myGame.TopCard;
+            if (top != null)
+            {
+                SwinGame.DrawText("Top Card: " + top.ToString(), Color.RoyalBlue, 0, 20);
+                SwinGame.DrawText("Player 1: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
+                SwinGame.DrawText("Player 2: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+                SwinGame.DrawCell(SwinGame.BitmapNamed("Cards"), top.CardIndex, 350, 50);
+            }
+            else
+            {
+                SwinGame.DrawText("Press SPACE to start!", Color.RoyalBlue, 0, 20);
+            }
 
-			//Draw onto the screen
-			SwinGame.RefreshScreen(60);
-		}
+            SwinGame.DrawCell(SwinGame.BitmapNamed("Cards"), 52, 160, 50);
+            SwinGame.RefreshScreen(60);
+        }
 
-		/// <summary>
-		/// Updates the game -- it should flip the cards itself once started!
-		/// </summary>
-		/// <param name="myGame">The game to be updated...</param>
-		private static void UpdateGame(Snap myGame)
-		{
-			myGame.Update(); // just ask the game to do this...
-		}
+        private static void UpdateGame(Snap myGame)
+        {
+            myGame.Update();
+        }
 
         public static void Main()
         {
-            //Open the game window
             SwinGame.OpenGraphicsWindow("Snap!", 860, 500);
-
-			//Load the card images and set their cell details
             LoadResources();
-            
-			// Create the game!
-			Snap myGame = new Snap ();
+            Snap myGame = new Snap();
 
-            //Run the game loop
-            while(false == SwinGame.WindowCloseRequested())
+            while (!SwinGame.WindowCloseRequested())
             {
-				HandleUserInput (myGame);
-				DrawGame (myGame);
-				UpdateGame (myGame);
+                HandleUserInput(myGame);
+                DrawGame(myGame);
+                UpdateGame(myGame);
             }
         }
     }
